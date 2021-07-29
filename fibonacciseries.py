b7ed6488993c3dfc8a8a667fb9d102a7e817007e
Tejshree Suresh <71011729+tejshreesuresh@users.noreply.github.com>
2021-07-30 00:15:30 +0530

    Update fibonacciseries.py

--- fibonacciseries.py
+++ fibonacciseries.py
@@ -3,7 +3,7 @@ a=int(input("Enter the number of terms: "))
 first=0                                         #first element of series
 second=1                                         #second element of series
 if a<=0:
-    print("The requested series is",f)
+    print("The requested series is",first)
 else:
     print(first,second, end=" ")
     for x in range(2,a):
